853fac428da0fcab141b88903ea49d2c57efd14a
MOHAMMAD SHOEB FAIZAN <shoebfaizan71@gmail.com>
2023-02-11 10:51:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let keyHasBeenPressed = () => {
+
+    let textAreaContent = document.querySelector('textarea');
+
+    let charCount = document.querySelector('.charCount');
+    charCount.innerHTML = String(textAreaContent.value).length;
+
+    let charLimit = 200;
+    let charLimitElement = document.querySelector('.charLimitSpan');
+    charLimitElement.innerHTML = charLimit - textAreaContent.value.length;
+
+    let wordCount = document.querySelector('.wordCount');
+
+    let wordsArray = String(textAreaContent.value).split(' ');
+
+    wordCount.innerHTML = wordsArray.length;
+
+    if(textAreaContent.value.length > 200){
+        textAreaContent.value = String(textAreaContent.value).substring(0, charLimit);
+    };
+};
+
+
+document.querySelector('.clearButton').addEventListener('click', ()=>{
+    document.querySelector('textarea').value = "";
+    document.querySelector('.charCount').innerHTML = 0;
+    document.querySelector('.wordCount').innerHTML = 0;
+    document.querySelector('.charLimitSpan').innerHTML = 0;
+});
